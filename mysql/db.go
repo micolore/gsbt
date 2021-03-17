1c04af47ca2c866f365fa9903ec54312d688a3fa
kubrick <471304402@qq.com>
2021-03-17 16:08:29 +0800

    feat(公共): udpate db init logic code
    udpate db init logic code

--- mysql/db.go
+++ mysql/db.go
@@ -9,35 +9,42 @@ import (
 
 var DB *gorm.DB
 
-// LocalHost
-func init() {
-	dbUserName := "root"
-	dbPWD := "root"
-	dbIP := "localhost"
-	dbPort := 3306
-	dbName := "test" //practice_questions
-	dbURL := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local", dbUserName, dbPWD, dbIP, dbPort, dbName)
-	var err error
-	DB, err = gorm.Open(mysql.Open(dbURL), &gorm.Config{
-		NamingStrategy: schema.NamingStrategy{
-			SingularTable: true,
-		},
-	})
-	if err != nil {
-		panic(err)
+var LocalDbName = "test"
+var LocalDbPWD = "root"
+var LocalDbUserName = "root"
+var LocalDbIP = "localhost"
+var LocalDbPort = 3306
+
+var TestFiveDbName = "wscrm"
+var TestFiveDbPWD = "123Moppo!@#"
+var TestFiveDbUserName = "root"
+var TestFiveDbIP = "192.168.1.5"
+var TestFiveDbPort = 3306
+
+var TestFourDbName = "scrm_bus"
+var TestFourDbPWD = "root"
+var TestFourDbUserName = "root"
+var TestFourDbIP = "192.168.1.4"
+var TestFourDbPort = 3306
+
+func getProfile(profileType int) string {
+	dbURL := ""
+	switch profileType {
+	// localhost
+	case 1:
+		dbURL = fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local", LocalDbUserName, LocalDbPWD, LocalDbIP, LocalDbPort, LocalDbName)
+	// 4
+	case 2:
+		dbURL = fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local", TestFourDbUserName, TestFourDbPWD, TestFourDbIP, TestFourDbPort, TestFourDbName)
+	// 5
+	case 3:
+		dbURL = fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local", TestFiveDbUserName, TestFiveDbPWD, TestFiveDbIP, TestFiveDbPort, TestFiveDbName)
 	}
-	if DB != nil {
-		fmt.Printf("db != nil\n")
-	}
+	return dbURL
 }
 
-func initFive() {
-	dbUserName := "root"
-	dbPWD := "123Moppo!@#"
-	dbIP := "192.168.1.5"
-	dbPort := 3306
-	dbName := "zhu"
-	dbURL := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local", dbUserName, dbPWD, dbIP, dbPort, dbName)
+func init() {
+	dbURL := getProfile(1)
 	var err error
 	DB, err = gorm.Open(mysql.Open(dbURL), &gorm.Config{
 		NamingStrategy: schema.NamingStrategy{
@@ -48,26 +55,6 @@ func initFive() {
 		panic(err)
 	}
 	if DB != nil {
-		fmt.Printf("db != nil\n")
-	}
-}
-func initFiveOff() {
-	dbUserName := "root"
-	dbPWD := "123Moppo!@#"
-	dbIP := "192.168.1.5"
-	dbPort := 3306
-	dbName := "wscrm"
-	dbURL := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local", dbUserName, dbPWD, dbIP, dbPort, dbName)
-	var err error
-	DB, err = gorm.Open(mysql.Open(dbURL), &gorm.Config{
-		NamingStrategy: schema.NamingStrategy{
-			SingularTable: true,
-		},
-	})
-	if err != nil {
-		panic(err)
-	}
-	if DB != nil {
-		fmt.Printf("db != nil\n")
+		fmt.Printf("db conn success!\n")
 	}
 }
