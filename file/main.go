36acbf26133dc62251f4278de45839808c4e71a8
kubrick <471304402@qq.com>
2021-09-26 14:53:22 +0800

    fix(公共): update file append error
    update file append error

--- file/main.go
+++ file/main.go
@@ -16,16 +16,18 @@ func main() {
 	//fmt.Println(content)
 	//writeFile(content)
 
-	path := "/Users/kubrick/Documents/moppo/code/peoject/scrm-2.0/whatsapp-scrm-server/scrm-admin-server-modules/"
-	GetAllFile(path)
+	// 1)
+	// path := "/Users/kubrick/Documents/moppo/code/peoject/scrm-2.0/whatsapp-scrm-server/scrm-admin-server-modules/"
+	// path = "/Users/kubrick/Documents/moppo/code/peoject/scrm-2.0/whatsapp-scrm-server/scrm-single-server-modules/"
+	// path = "/Users/kubrick/Documents/moppo/code/peoject/scrm-2.0/whatsapp-scrm-server/scrm-biz-modules/"
+	// path = "/Users/kubrick/Documents/moppo/code/peoject/scrm-2.0/whatsapp-scrm-server/scrm-common-modules/"
 
-	path = "/Users/kubrick/Documents/moppo/code/peoject/scrm-2.0/whatsapp-scrm-server/scrm-biz-modules/"
+	// 2)
+	path := "/Users/kubrick/Documents/moppo/code/peoject/marketing-treasure-server/marketing-treasure-server/marketing-api/"
 	GetAllFile(path)
-
-	path = "/Users/kubrick/Documents/moppo/code/peoject/scrm-2.0/whatsapp-scrm-server/scrm-common-modules/"
+	path = "/Users/kubrick/Documents/moppo/code/peoject/marketing-treasure-server/marketing-common-modules/"
 	GetAllFile(path)
-
-	path = "/Users/kubrick/Documents/moppo/code/peoject/scrm-2.0/whatsapp-scrm-server/scrm-single-server-modules/"
+	path = "/Users/kubrick/Documents/moppo/code/peoject/marketing-treasure-server/marketing-treasure-server/marketing-manager/"
 	GetAllFile(path)
 
 }
@@ -43,20 +45,19 @@ func GetAllFile(path string) {
 	for _, fi := range rd {
 		fn := fi.Name()
 		if fi.IsDir() {
-			GetAllFile(path + fn + "//")
+			GetAllFile(path + fn + "/")
 		} else {
 			if strings.Contains(fn, ".java") {
 				fullName := path + fn
-				fmt.Println(fullName)
 				content := readSingleFile(fullName)
+				fmt.Println(fullName)
 				writeFile(content)
 			}
 		}
 	}
 }
-
 func writeFile(content string) {
-	outputFile, outputError := os.OpenFile("/Users/kubrick/Documents/20210909-code.txt", os.O_WRONLY|os.O_CREATE, 0666)
+	outputFile, outputError := os.OpenFile("/Users/kubrick/Documents/20210926-code.txt", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
 	if outputError != nil {
 		fmt.Printf("An error occurred with file opening or creation\n")
 		return
@@ -65,6 +66,30 @@ func writeFile(content string) {
 	outputWriter := bufio.NewWriter(outputFile)
 	outputWriter.WriteString(content)
 	outputWriter.Flush()
+}
+
+func writeFileV2(content string) {
+	fileName := "/Users/kubrick/Documents/20210926-code.txt"
+	var outputFile *os.File
+
+	outputFile, err := os.OpenFile(fileName, os.O_RDONLY|os.O_WRONLY, 0666)
+	if err != nil {
+		fmt.Println("Open file err =", err)
+		return
+	}
+	if outputFile == nil {
+		var outputError error
+		outputFile, outputError = os.OpenFile(fileName, os.O_WRONLY, 0644)
+		if outputError != nil {
+			fmt.Printf("An error occurred with file opening or creation\n")
+			return
+		}
+	}
+	defer outputFile.Close()
+	_, err = io.WriteString(outputFile, content)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func readSingleFile(filePath string) string {
