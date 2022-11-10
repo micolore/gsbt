5c7787f0e9ab9f307e3c5402c34b93ef8a5ad2f2
chenchao <471304402@qq.com>
2022-11-10 21:37:12 +0800

    feat(公共): add oss
    add oss

--- go.mod
+++ go.mod
@@ -3,53 +3,41 @@ module moppo.com/gsbt
 go 1.15
 
 require (
-	code.sajari.com/docconv v1.1.0 // indirect
 	github.com/ClickHouse/clickhouse-go v1.5.2 // indirect
 	github.com/ClickHouse/clickhouse-go/v2 v2.0.0-beta.5
-	github.com/CodyGuo/dingtalk v0.0.3 // indirect
-	github.com/CodyGuo/glog v1.0.6 // indirect
-	github.com/JalfResi/justext v0.0.0-20170829062021-c0282dea7198 // indirect
-	github.com/RoaringBitmap/roaring v0.9.4 // indirect
+	github.com/CodyGuo/dingtalk v0.0.3
+	github.com/CodyGuo/glog v1.0.6
+	github.com/RoaringBitmap/roaring v0.9.4
 	github.com/Shopify/sarama v1.28.0
-	github.com/advancedlogic/GoOse v0.0.0-20200830213114-1225d531e0ad // indirect
-	github.com/aliyun/aliyun-oss-go-sdk v2.1.8+incompatible
+	github.com/aliyun/aliyun-oss-go-sdk v2.2.5+incompatible
 	github.com/apache/rocketmq-client-go/v2 v2.1.0
 	github.com/bits-and-blooms/bitset v1.2.1 // indirect
-	github.com/bsm/sarama-cluster v2.1.15+incompatible // indirect
 	github.com/dcu/pdf v0.0.0-20190612170416-c8b299d05f89
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
 	github.com/go-echarts/go-echarts/v2 v2.2.3
-	github.com/go-redis/redis/v8 v8.9.0 // indirect
-	github.com/go-resty/resty/v2 v2.7.0 // indirect
+	github.com/go-redis/redis/v8 v8.9.0
 	github.com/golang-module/carbon v1.5.5
 	github.com/golang/mock v1.6.0 // indirect
-	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/google/go-tika v0.2.0 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
 	github.com/google/gopacket v1.1.19
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/labstack/echo/v4 v4.5.0
-	github.com/ledongthuc/pdf v0.0.0-20220302134840-0c2507a12d80 // indirect
-	github.com/levigross/exp-html v0.0.0-20120902181939-8df60c69a8f5 // indirect
-	github.com/manifoldco/promptui v0.9.0 // indirect
-	github.com/olivere/elastic/v7 v7.0.24 // indirect
-	github.com/panjf2000/ants/v2 v2.5.0 // indirect
-	github.com/pelletier/go-toml v1.9.4 // indirect
+	github.com/manifoldco/promptui v0.9.0
+	github.com/olivere/elastic/v7 v7.0.24
+	github.com/panjf2000/ants/v2 v2.5.0
+	github.com/rogpeppe/go-internal v1.8.0 // indirect
 	github.com/rsocket/rsocket-go v0.8.4
 	github.com/sirupsen/logrus v1.8.1 // indirect
-	github.com/spf13/cast v1.4.1 // indirect
-	github.com/spf13/cobra v1.2.1
-	github.com/spf13/viper v1.8.1
+	github.com/spf13/cobra v1.6.1
+	github.com/spf13/viper v1.8.1 // indirect
 	github.com/tidwall/gjson v1.12.1 // indirect
-	github.com/unidoc/unioffice v1.9.0 // indirect
-	github.com/unidoc/unipdf/v3 v3.40.0 // indirect
-	github.com/xuri/excelize/v2 v2.4.1 // indirect
 	go.etcd.io/etcd/client/v3 v3.5.1
+	go.opentelemetry.io/otel/internal/metric v0.27.0 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/zap v1.19.1
+	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa // indirect
 	golang.org/x/net v0.1.0 // indirect
 	golang.org/x/time v0.0.0-20210220033141-f8bda1e9f3ba // indirect
-	gopkg.in/ini.v1 v1.63.0 // indirect
 	gorm.io/driver/mysql v1.0.5
 	gorm.io/gorm v1.21.3
-	mvdan.cc/sh/v3 v3.3.1 // indirect
 )
--- oss/main.go
+++ oss/main.go
@@ -3,38 +3,129 @@ package main
 import (
 	"fmt"
 	"github.com/aliyun/aliyun-oss-go-sdk/oss"
+	"io/ioutil"
 	"os"
+	"path"
+	"strings"
 )
+
+// oss 上传文件
+// 1、命令参数-指定目录下面文件，上传到指定目录下面
+// 2、新增配置文件配置oss配置信息
+// 3、指定文件上传到指定目录下面
+func main() {
+	files, err := GetAllFiles("/Users/kubrick/Documents/项目文档")
+	if err != nil {
+		panic(err)
+	}
+	for i := 0; i < len(files); i++ {
+		file := files[i]
+		fmt.Println(files[i])
+		// linux
+		// uploadFile(strings.TrimPrefix(file, "/"), file)
+
+		// windows
+		//获取文件名带后缀
+		filenameWithSuffix := path.Base(file)
+		uploadFile("kubrick/20221110/"+filenameWithSuffix, file)
+
+	}
+}
 
 func handleError(err error) {
 	fmt.Println("Error:", err)
 	os.Exit(-1)
 }
 
-func main() {
-	// Endpoint以杭州为例，其它Region请按实际情况填写。
-	endpoint := "oss-ap-southeast-1.aliyuncs.com"
-	// 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建RAM账号。
-	accessKeyId := ""
-	accessKeySecret := ""
-	bucketName := ""
-	// <yourObjectName>上传文件到OSS时需要指定包含文件后缀在内的完整路径，例如abc/efg/123.jpg。
-	objectName := ""
-	// <yourLocalFileName>由本地文件路径加文件名包括后缀组成，例如/users/local/myfile.txt。
-	localFileName := ""
-	// 创建OSSClient实例。
+const endpoint = "oss-cn-shanghai.aliyuncs.com"
+const accessKeyId = ""
+const accessKeySecret = ""
+const bucketName = "one-return"
+
+func uploadFile(objectName, localFileName string) {
 	client, err := oss.New(endpoint, accessKeyId, accessKeySecret)
 	if err != nil {
 		handleError(err)
 	}
-	// 获取存储空间。
 	bucket, err := client.Bucket(bucketName)
 	if err != nil {
 		handleError(err)
 	}
-	// 上传文件。
 	err = bucket.PutObjectFromFile(objectName, localFileName)
 	if err != nil {
 		handleError(err)
 	}
+}
+
+// GetFilesAndDirs 获取指定目录下的所有文件和目录
+func GetFilesAndDirs(dirPth string) (files []string, dirs []string, err error) {
+	dir, err := ioutil.ReadDir(dirPth)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	PthSep := string(os.PathSeparator)
+	//suffix = strings.ToUpper(suffix) //忽略后缀匹配的大小写
+
+	for _, fi := range dir {
+		if fi.IsDir() { // 目录, 递归遍历
+			dirs = append(dirs, dirPth+PthSep+fi.Name())
+			GetFilesAndDirs(dirPth + PthSep + fi.Name())
+		} else {
+			// 过滤指定格式
+			ok := strings.HasSuffix(fi.Name(), ".go")
+			if ok {
+				files = append(files, dirPth+PthSep+fi.Name())
+			}
+		}
+	}
+
+	return files, dirs, nil
+}
+
+// GetAllFiles 获取指定目录下的所有文件,包含子目录下的文件
+func GetAllFiles(dirPth string) (files []string, err error) {
+	var dirs []string
+	dir, err := ioutil.ReadDir(dirPth)
+	if err != nil {
+		return nil, err
+	}
+
+	PthSep := string(os.PathSeparator)
+	//suffix = strings.ToUpper(suffix) //忽略后缀匹配的大小写
+
+	for _, fi := range dir {
+		fileName := fi.Name()
+		if fi.IsDir() { // 目录, 递归遍历
+			dirs = append(dirs, dirPth+PthSep+fileName)
+			GetAllFiles(dirPth + PthSep + fileName)
+		} else {
+			files = append(files, dirPth+PthSep+fileName)
+		}
+	}
+
+	// 读取子目录下文件
+	for _, table := range dirs {
+		temp, _ := GetAllFiles(table)
+		for _, temp1 := range temp {
+			files = append(files, temp1)
+		}
+	}
+	return files, nil
+}
+
+// filterFile 过滤指定文件名称
+func filterFile(fileName string) bool {
+	ok := strings.HasSuffix(fileName, ".go")
+	if ok {
+		return true
+	}
+	return false
+}
+
+// /usr/1.png usr/1.png
+// fileNameToObjectName 文件名称转objectName
+func fileNameToObjectName(fileName string) string {
+
+	return ""
 }
